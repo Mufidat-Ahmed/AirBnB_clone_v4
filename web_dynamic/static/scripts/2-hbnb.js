5cfbe9debf0928108e8ebc9e29d293c6b603fe29
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-12-10 20:44:53 -0800

    Last clone

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,15 @@
+// static/scripts/1-hbnb.js
+
+$(document).ready(function() {
+	function updateApiStatus() {
+		$.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+			if (data.status === "OK") {
+				$("#api_status").addClass("available");
+			} else {
+				$("#api_status").removeClass("available");
+			}
+		});
+	}
+	updateApiStatus();
+	setInterval(updateApiStatus, 5000);
+});
